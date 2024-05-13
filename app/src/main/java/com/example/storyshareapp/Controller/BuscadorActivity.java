45a4d594ea04fdd1bac0e9da542225098fcfcbfa
Magali <magalii.martinez@gmail.com>
2024-05-13 11:12:51 +0200

    pantalla creada

---
+++ app/src/main/java/com/example/storyshareapp/Controller/BuscadorActivity.java
@@ -0,0 +1,22 @@
+package com.example.storyshareapp.Controller;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.storyshareapp.R;
+
+public class BuscadorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_buscador);
+
+    }
+}
