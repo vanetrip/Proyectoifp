fe54d9de1b0846d40cae41404d0e48457ff9d7dd
Magali <magalii.martinez@gmail.com>
2024-05-13 12:26:09 +0200

    refresh

--- app/src/main/java/com/example/storyshareapp/Controller/BuscadorActivity.java
+++ app/src/main/java/com/example/storyshareapp/Controller/BuscadorActivity.java
@@ -1,6 +1,7 @@
 package com.example.storyshareapp.Controller;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 
@@ -18,11 +19,20 @@ public class BuscadorActivity extends AppCompatActivity {
     private ImageView image2;
     private ImageView image3;
     private ListView list1;
+    private Button boton1;
+    private Button boton2;
+    private Button boton3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_buscador);
-
+        image1 = (ImageView) findViewById(R.id.imageView3_buscador);
+        image2 = (ImageView) findViewById(R.id.imageView4_buscador);
+        image3 = (ImageView) findViewById(R.id.imageView5_buscador);
+        boton1 = (Button) findViewById(R.id.button1_buscador);
+        boton2 = (Button) findViewById(R.id.button7_buscador);
+        boton3 = (Button) findViewById(R.id.button8_buscador);
+        list1 = (ListView) findViewById(R.id.listView_buscador);
     }
 }
