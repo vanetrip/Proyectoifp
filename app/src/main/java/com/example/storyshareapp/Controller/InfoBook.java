ed0ae5bbfc506cecd27dea5196d738356d14a43f
Magali <magalii.martinez@gmail.com>
2024-05-18 16:15:04 +0200

    configurada portada

--- app/src/main/java/com/example/storyshareapp/Controller/InfoBook.java
+++ app/src/main/java/com/example/storyshareapp/Controller/InfoBook.java
@@ -2,16 +2,26 @@ package com.example.storyshareapp.Controller;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.icu.text.IDNA;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.Target;
 import com.example.storyshareapp.Model.Eventos;
 import com.example.storyshareapp.Persistencia.BasedeDatos;
 import com.example.storyshareapp.Persistencia.Evento;
@@ -29,6 +39,8 @@ public class InfoBook extends AppCompatActivity {
     private ImageView image2;
     private ImageView image3;
     private ImageView image4;
+    private ImageView image5;
+
 
     private TextView textView1;
     private TextView textView2;
@@ -39,7 +51,7 @@ public class InfoBook extends AppCompatActivity {
     private int idUsuario;
     private int idLibro;
     private int idEvento;
-    private String paquete1="";
+    private String paquete1 = "";
     private Bundle extras;
 
     @SuppressLint("MissingInflatedId")
@@ -55,6 +67,7 @@ public class InfoBook extends AppCompatActivity {
         image2 = (ImageView) findViewById(R.id.imageView4_info_book);
         image3 = (ImageView) findViewById(R.id.imageView5_info_book);
         image4 = (ImageView) findViewById(R.id.imageView1_info_book);
+        image5 = (ImageView) findViewById(R.id.imageView6_infoBook);
         textView1 = (TextView) findViewById(R.id.textView5_info_book);
         textView2 = (TextView) findViewById(R.id.textView10_infoBook);
         textView3 = (TextView) findViewById(R.id.textView12_infoBook);
@@ -66,24 +79,24 @@ public class InfoBook extends AppCompatActivity {
         // Obtener el idUsuario del Intent que inició esta actividad
         Intent intent = getIntent();
         idUsuario = intent.getIntExtra("idUsuario", -1); // -1 es un valor predeterminado en caso de que no se encuentre el extra
-        System.out.println("idUsuario "+idUsuario);
+        System.out.println("idUsuario " + idUsuario);
         //Obtner el idLibro
         idLibro = intent.getIntExtra("idLibro", -1);
-        System.out.println("idLibro "+idLibro);
+        System.out.println("idLibro " + idLibro);
 
         Libro libro = basedeDatos.obtenerLibro(idLibro);
 
         if (libro != null) {
+            cargarImagenPortada(libro.getPortada(), image5);
             textView1.setText(libro.getTitulo()); // Título del libro - TITULO
             textView2.setText(libro.getTitulo()); // Título del libro - DESCRIPCION
             textView3.setText(libro.getAutor()); // Autor del libro - DESCRIPCION
-            //textView4.setText(libro.getTitulo()); // FECHA EVENTO - DESCRIPCION
-            //textView5.setText(libro.getTitulo()); // HORA EVENTO - DESCRIPCION
+
         } else {
             Toast.makeText(this, "No se encontró información del libro", Toast.LENGTH_SHORT).show();
         }
 
-        Evento evento =basedeDatos.obtenerEvento(idEvento);
+        Evento evento = basedeDatos.obtenerEvento(idEvento);
         if (evento != null) {
             textView4.setText(libro.getTitulo()); // FECHA EVENTO - DESCRIPCION
             textView5.setText(libro.getTitulo()); // HORA EVENTO - DESCRIPCION
@@ -167,8 +180,50 @@ public class InfoBook extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-
     }
+
+        private void cargarImagenPortada(String urlPortada, ImageView imageView) {
+            if (urlPortada != null && !urlPortada.isEmpty()) {
+                RequestOptions requestOptions = new RequestOptions()
+                        .placeholder(R.drawable.logo_blanco)
+                        .error(R.drawable.avatar_blanco)
+                        .diskCacheStrategy(DiskCacheStrategy.ALL);
+
+                Glide.with(this)
+                        .load(urlPortada)
+                        .apply(requestOptions)
+                        .listener(new RequestListener<Drawable>() {
+                            @Override
+                            public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                                // Manejo de errores al cargar la imagen
+                                Log.e("TAG", "Error al cargar la imagen: " + e.getMessage(), e);
+                                // Puedes mostrar un mensaje de error al usuario si lo deseas
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(InfoBook.this, "Error al cargar la imagen", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                                return false; // Retornar 'true' si deseas que Glide maneje el error y cargue la imagen de error definida
+                            }
+
+                            @Override
+                            public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                                // La imagen se cargó exitosamente
+                                Log.d("TAG", "Imagen cargada con éxito");
+                                return false;
+                            }
+                        })
+                        .into(imageView);
+            } else {
+                // La URL de la imagen es inválida
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(InfoBook.this, "URL de portada no válida", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+                        }
 
 }
